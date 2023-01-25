46135b1da0984289383146a9f26b3457a972d92b
Cortney Thomas <thomas.cortney@gmail.com>
2023-01-25 16:55:01 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `portfolio`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "JWvSfiF983p7ZNZTUuy2JZwmAasryIs13c04zn4sEXA",
+      "spaceId": "j9waukffttug"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-sass"]
+};
