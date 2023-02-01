b93a5cc864da4dfd52a84b329642a966b3fbae1e
Cortney Thomas <thomas.cortney@gmail.com>
2023-02-01 12:14:17 -0500

    add base pages

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import * as React from "react"
+
+const AboutPage = () => {
+    return (
+        <main>
+            <header>
+                <p>About</p>
+                <h1>Cortney Thomas</h1>
+                <h2>Developer & Psychology Nerd</h2>
+            </header>
+      </main>
+  )
+}
+
+export default AboutPage
+
+export const Head = () => <title>About</title>
---
+++ src/pages/projects.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+
+const ProjectsPage = () => {
+    return (
+        <main>
+            <header>
+                <p>Projects</p>
+            </header>
+      </main>
+  )
+}
+
+export default ProjectsPage
+
+export const Head = () => <title>Projects</title>
