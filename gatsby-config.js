054c4fe255b5e034dec77e1625b051fbe06cd178
Cortney Thomas <thomas.cortney@gmail.com>
2023-02-06 11:16:04 -0500

    one of these days

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,8 +10,8 @@ module.exports = {
   plugins: [{
     resolve: 'gatsby-source-contentful',
     options: {
-      "accessToken": process.env.CONTENTFUL_ACCESS_TOKEN,
-      "spaceId": process.env.CONTENTFUL_SPACE_ID
+      "accessToken": `${process.env.CONTENTFUL_ACCESS_TOKEN}`,
+      "spaceId": `${process.env.CONTENTFUL_SPACE_ID}`
     }
   }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-sass"]
 };
