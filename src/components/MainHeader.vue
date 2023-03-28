d27d4ef8c380a2b464d0738752b661fe4c112b71
Cortney Thomas <thomas.cortney@gmail.com>
2023-03-28 18:29:39 -0400

    add tailwind and postcss

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <header>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/contact">Contact</RouterLink>
+      </nav>
+  </header>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
