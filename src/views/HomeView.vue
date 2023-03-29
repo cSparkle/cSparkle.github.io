739aa2589d6efa5a42f3c7737a0ca9375b843dcb
Cortney Thomas <thomas.cortney@gmail.com>
2023-03-29 12:30:13 -0400

    style mobile homepage and add arrow icon

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,40 @@
 <template>
   <main>
-    <h2>Howdy friend</h2>
-    <h3>The name's Cortney. I'm a</h3>
+    <h2 class="text-4xl text-center font-mono text-bright-green">Howdy, friend</h2>
+    <h3 class="text-3xl text-center font-sans mb-12">The name's Cortney.</h3>
     <section>
-      <div>
-        <p>Software Engineer</p>
+      <div class="flex flex-col items-center justify-center">
         <img
           src="../assets/images/developer.jpg"
           alt="top down vector drawing of hands coding on a laptop"
+          class="w-64"
         />
+        <p class="text-dark-grey font-sans font-medium text-xl flex items-center">
+          Software Engineer
+          <img src="../assets/icons/arrow-right-solid.svg" alt="right arrow" class="h-6 ml-2.5" />
+        </p>
       </div>
-      <div>
-        <p>Bookworm</p>
+      <div class="flex flex-col items-center justify-center my-16">
         <img
           src="../assets/images/bookworm.jpg"
           alt="front on vector drawing of glasses on top of stacked books next to a coffee mug"
+          class="w-64"
         />
+        <p class="text-dark-grey font-sans font-medium text-xl flex items-center">
+          Bookworm
+          <img src="../assets/icons/arrow-right-solid.svg" alt="right arrow" class="h-6 ml-2.5" />
+        </p>
       </div>
-      <div>
-        <p>Fantasy Nerd</p>
+      <div class="flex flex-col items-center justify-center my-16">
         <img
           src="../assets/images/fantasynerd.jpg"
           alt="vector drawing of four costumed individuals at a gaming convention"
+          class="w-64"
         />
+        <p class="text-dark-grey font-sans font-medium text-xl flex items-center">
+          Fantasy Nerd
+          <img src="../assets/icons/arrow-right-solid.svg" alt="right arrow" class="h-6 ml-2.5" />
+        </p>
       </div>
     </section>
   </main>
