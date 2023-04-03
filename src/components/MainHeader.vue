c2f1e7734c58604ceef98fe5cfd3f510b1f50e40
Cortney <thomas.cortney@gmail.com>
2023-04-03 10:17:55 -0400

    why you be this way header

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,6 +1,6 @@
 <template>
-  <header class="h-24 w-screen fixed bg-dark-grey">
-    <nav class="flex justify-between items-center p-4">
+  <header class="h-24 w-screen sticky top-0 bg-dark-grey p-4">
+    <nav class="flex justify-between items-center">
       <img
         src="../assets/images/cat-logo.svg"
         alt="vector drawing of cat in spacesuit floating through space"
@@ -58,17 +58,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-.logo-circle {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 3rem;
-  width: 3rem;
-  background: #5ae3a9;
-  border-radius: 50%;
-  box-shadow: -20px -20px 25px #fff, 3px 3px 20px #a6a8a8, inset -3px -3px 10px #5ae3a9;
-  position: relative;
-}
-</style>
